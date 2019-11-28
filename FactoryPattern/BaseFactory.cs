c33dbfe707e7440ac1e345f1bb6e55fdf2d890b4
og <obed1355@gmail.com>
2019-11-27 22:15:50 -0400

    added base pattern structure

---
+++ FactoryPattern/BaseFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryPattern
+{
+    class BaseFactory
+    {
+        /// <summary>
+        /// Main product interface. Objects that the factory method will create.
+        /// </summary>
+        abstract class Product
+        {
+        }
+
+        /// <summary>
+        /// Implements the product.
+        /// </summary>
+        class ConcreteProduct : Product
+        {
+        }
+
+        /// <summary>
+        /// Contains the implementations for methods to manipulate products.
+        /// Declares factory method that returns an object of type product.
+        /// </summary>
+        class Creator
+        {
+        }
+
+        /// <summary>
+        /// Overrides the factory method to return an instance of a concrete product.
+        /// </summary>
+        class ConcreteCreator
+        {
+        }
+    }
+}
